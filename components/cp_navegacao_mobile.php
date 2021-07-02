fb969822d94da8e6e2130fb0c2c42511b3405952
Miguel <miguel.melo.teixeira@ua.pt>
2021-07-02 11:44:28 +0100

    02/07/2021 - alterações navegação adicionada ao icon da home page

--- components/cp_navegacao_mobile.php
+++ components/cp_navegacao_mobile.php
@@ -1,7 +1,9 @@
 <nav class="container-fluid nav-mobile display-md-off display-xs-block">
     <div class="row nav-mobile-row">
         <site class="col-4 text-center">
-            <img src="assets/img/nav_home.svg">
+            <a href="home_page.php">
+                <img src="assets/img/nav_home.svg">
+            </a>
         </site>
         <site class="col-4 text-center">
             <img src="assets/img/nav_nucleo.svg">
