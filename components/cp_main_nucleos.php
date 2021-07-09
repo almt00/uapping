6325f66ddc98fb59290b579dd080014be839f4e3
ana <almt00@ua.pt>
2021-07-09 20:13:07 +0100

    09/07/2021 - nucleos + DB

--- components/cp_main_nucleos.php
+++ components/cp_main_nucleos.php
@@ -19,7 +19,8 @@
                         <border class="checkbox-top-home-page_overlay"></border>
                         <selector id="selector" class="selector">
                         </selector>
-                        <section style="height:100%;" class="row sec-selector-home-page text-center align-content-center">
+                        <section style="height:100%;"
+                                 class="row sec-selector-home-page text-center align-content-center">
                             <article id="oficiais" class="switch-interesses col-6">
                                 <p> Oficiais </p>
                             </article>
@@ -45,35 +46,44 @@
                 <article class="col-12 px-4">
 
                     <section class="row mb-3">
-
-                        <article class="col-6 art-card-nucleo_geral" style="padding-right:8px;">
-                            <a href="nucleos_detail.php">
-                                <div class="nucleo_card" style="background-image: ;">
-                                    <div class="row align-items-center sec_nucleo_card_img">
-                                        <div class="col-4 art_nucleo_card min-nucleo-card">
-                                            <img src="assets/img/user_profile.png">
-                                        </div>
-                                        <div class="col-6 art_nucleo_card word-warp">
-                                            <p class="p-nucleo_name m-0"> NFC </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </article>
-                        <article class="col-6 art-card-nucleo_geral" style="padding-left:8px;">
-                            <a href="nucleos_detail.php">
-                                <div class="nucleo_card" style="background-image: ;">
-                                    <div class="row align-items-center sec_nucleo_card_img">
-                                        <div class="col-4 art_nucleo_card min-nucleo-card">
-                                            <img src="assets/img/user_profile.png">
-                                        </div>
-                                        <div class="col-6 art_nucleo_card word-warp">
-                                            <p class="p-nucleo_name m-0"> NROCK </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </article>
+                        <?php
+                        require_once "connections/connection.php";
+                        $link = new_db_connection();
+                        $stmt = mysqli_stmt_init($link);
+                        $query = "SELECT nucleos.id_nucleo,nucleos.nome_nucleo,nucleos.sigla_nucleo FROM nucleos
+                                INNER JOIN nucleos_oficiais
+                                ON nucleos.id_nucleo=nucleos_oficiais.ref_id_nucleo;";
+                        if (mysqli_stmt_prepare($stmt, $query)) {
+                            if (mysqli_stmt_execute($stmt)) {
+                                mysqli_stmt_bind_result($stmt, $id_nucleo, $nome_nucleo, $sigla_nucleo);
+                                while (mysqli_stmt_fetch($stmt)) {
+                                    ?>
+                                    <article class="col-6 art-card-nucleo_geral" style="padding-right:8px;">
+                                        <a href="nucleos_detail.php?id_nucleo=<?=$id_nucleo?>">
+                                            <div class="nucleo_card" style="background-image: ;">
+                                                <div class="row align-items-center sec_nucleo_card_img">
+                                                    <div class="col-4 art_nucleo_card min-nucleo-card">
+                                                        <img src="assets/img/user_profile.png">
+                                                    </div>
+                                                    <div class="col-6 art_nucleo_card word-warp">
+                                                        <p class="p-nucleo_name m-0"> <?=$sigla_nucleo?> </p>
+                                                        <p class="p-nucleo_name m-0"> <?=$nome_nucleo?> </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </article>
+                                    <?php
+                                }
+                            } else {
+                                echo "Error:" . mysqli_stmt_error($stmt);
+                            }
+                            mysqli_stmt_close($stmt);
+                        } else {
+                            echo("Error description: " . mysqli_error($link));
+                        }
+                        mysqli_close($link);
+                        ?>
 
                     </section>
 
@@ -83,13 +93,15 @@
     </section>
     <footer class="row justify-content-center py-5">
         <article class="col-3 text-center">
-            <a href="https://www.facebook.com/" target="_blank"> <span class="fab fa-facebook-f text-white fa-3x"></span> </a>
+            <a href="https://www.facebook.com/" target="_blank"> <span
+                        class="fab fa-facebook-f text-white fa-3x"></span> </a>
         </article>
         <article class="col-3 text-center mw-6rem">
             <a href="https://twitter.com/" target="_blank"> <span class="fab fa-twitter text-white fa-3x"></span> </a>
         </article>
         <article class="col-3 text-center">
-            <a href="https://www.instagram.com/" target="_blank"> <span class="fab fa-instagram text-white fa-3x"></span> </a>
+            <a href="https://www.instagram.com/" target="_blank"> <span
+                        class="fab fa-instagram text-white fa-3x"></span> </a>
         </article>
     </footer>
 </main>
