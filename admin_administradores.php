f4606aa85baef7f0968d55ef695eb043959a8b1d
Miguel <miguel.melo.teixeira@ua.pt>
2021-07-11 21:30:56 +0100

    11/07/21 - parte da página dos administradores feita (falta ver mais tarde a tabela dos users)

---
+++ admin_administradores.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+
+    <?php include_once "helpers/help_css.php"?>
+
+    <?php include_once "helpers/help_meta.php"?>
+
+</head>
+<body>
+
+<script> admin_user = true; </script>
+
+<?php $nav_nucleo = true; ?>
+
+<?php include_once "components/cp_header.php" ?>
+
+<?php include_once "components/cp_admin_administradores.php" ?>
+
+<?php include_once "components/cp_navegacao_mobile.php" ?>
+
+<?php include_once "helpers/help_js.php"?>
+
+</body>
+</html>
---
+++ components/cp_admin_administradores.php
@@ -0,0 +1,36 @@
+<main class="container-fluid main-flex">
+    <section class="row">
+        <article class="col-12">
+            <section class="row sec-top-nucleos">
+                <article class="col-12 section-search-home-page-admin">
+                    <section class="row justify-content-center">
+                        <article class="col-12 px-4">
+                        </article>
+                    </section>
+                </article>
+            </section>
+            <section class="row section-nucleos">
+                <article class="col-12 mt-4 mb-3 px-4">
+                    <h2 class="h2-admin-administradores"> Gestão <span> Gretua </span> </h2>
+                </article>
+                <article class="col-12 mt-4 mb-3 px-4">
+                    <h2 class="h2-admin-administradores-sub d-inline-block mr-2"> Administradores </h2>
+                    <a href="admin_novo_admin.php"><img class="img-add-administradores" src="assets/admin/add_administradores.svg"></a>
+                </article>
+            </section>
+        </article>
+    </section>
+    <footer class="row justify-content-center py-5">
+        <article class="col-3 text-center">
+            <a href="https://www.facebook.com/" target="_blank"> <span
+                        class="fab fa-facebook-f text-white fa-3x"></span> </a>
+        </article>
+        <article class="col-3 text-center mw-6rem">
+            <a href="https://twitter.com/" target="_blank"> <span class="fab fa-twitter text-white fa-3x"></span> </a>
+        </article>
+        <article class="col-3 text-center">
+            <a href="https://www.instagram.com/" target="_blank"> <span
+                        class="fab fa-instagram text-white fa-3x"></span> </a>
+        </article>
+    </footer>
+</main>
