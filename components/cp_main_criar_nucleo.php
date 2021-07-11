b625c4d6ff96725be759c9221170a39a7d3a7d09
ana <almt00@ua.pt>
2021-07-11 12:57:31 +0100

    11/07/2021 - PHP para criar nucleo fantasma

--- components/cp_main_criar_nucleo.php
+++ components/cp_main_criar_nucleo.php
@@ -14,21 +14,22 @@
                     <h2 class="text-center h2-nucleo_save"> Criar um Núcleo </h2>
                 </article>
                 <article class="col-12 px-4">
-                    <form action="" method="post" id="criar_nucleo">
+                    <form action="scripts/sc_criar_nucleo.php" method="post" id="criar_nucleo">
                         <section class="row justify-content-center">
                             <article class="col-12">
-                                <select required="" class="custom-select select_criar_nucleo mb-3 mb-md-3" id="departamentos"
-                                        name="departamento" form="sign_up">
+                                <select required="required" class="custom-select select_criar_nucleo mb-3 mb-md-3"
+                                        id="area"
+                                        name="area" form="criar_nucleo">
                                     <?php
                                     require_once "connections/connection.php";
                                     $link = new_db_connection();
                                     $stmt = mysqli_stmt_init($link);
-                                    $query = "SELECT id_departamento, nome_departamento FROM departamentos";
+                                    $query = "SELECT id_interesse, nome_interesse FROM interesses";
                                     if (mysqli_stmt_prepare($stmt, $query)) {
                                         if (mysqli_stmt_execute($stmt)) {
-                                            mysqli_stmt_bind_result($stmt, $id_departamento, $nome_departamento);
+                                            mysqli_stmt_bind_result($stmt, $id_interesse, $nome_interesse);
                                             while (mysqli_stmt_fetch($stmt)) {
-                                                echo '<option value="' . $id_departamento . '">' . $nome_departamento . '</option>';
+                                                echo '<option value="' . $id_interesse . '">' . $nome_interesse . '</option>';
                                             }
                                         } else {
                                             echo "Error:" . mysqli_stmt_error($stmt);
@@ -40,16 +41,19 @@
                                     ?>
                                 </select>
                                 <input id="nome" class="input_criar_nucleo mb-3 mb-md-3" type="text" name="nome"
-                                       size="24" placeholder="nome">
+                                       size="24" placeholder="nome" required="required">
                                 <input id="sigla" class="input_criar_nucleo mb-3 mb-md-3" type="text" name="sigla"
-                                       size="24" placeholder="sigla">
-                                <textarea placeholder="descrição" class="form-control text-area-criar-nucleo" id="exampleFormControlTextarea1" rows="5"></textarea>
+                                       size="24" placeholder="sigla" required="required">
+                                <textarea required="required" name="descricao" placeholder="descrição"
+                                          class="form-control text-area-criar-nucleo" id="exampleFormControlTextarea1"
+                                          rows="5"></textarea>
                             </article>
                         </section>
                         <section class="row justify-content-center mt-3 mt-md-3">
                             <article class="col-md-12 mt-3 mt-md-5 px-4">
-                                <input type="submit" class="mb-2 mb-md-2" style="display: block;" value="Submeter" id="criar_nucleo_submit">
-                                <button id="cancelar_criar_nucleo" class="mb-5" type="button"> cancelar </button>
+                                <input form="criar_nucleo" type="submit" class="mb-2 mb-md-2" style="display: block;"
+                                       value="Submeter" id="criar_nucleo_submit">
+                                <button id="cancelar_criar_nucleo" class="mb-5" type="button"> cancelar</button>
                             </article>
                         </section>
                     </form>
