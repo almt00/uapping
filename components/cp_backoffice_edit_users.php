d183a3bb7083334364a27a5bf7ff306e2cf05eac
Miguel <miguel.melo.teixeira@ua.pt>
2021-07-14 19:49:49 +0100

    14/07/21 - página de users, edição dos users e home_page do backoffice feita. Inclusive um avanço na navegação mobile do backoffice

---
+++ components/cp_backoffice_edit_users.php
@@ -0,0 +1,118 @@
+<main class="container-fluid main-flex overflow-hidden">
+    <section class="row">
+        <article class="col-12">
+            <section class="row sec-top-nucleos">
+                <article class="col-12 section-search-home-page-backoffice">
+                    <section class="row justify-content-center">
+                        <article class="col-12 px-4">
+                        </article>
+                    </section>
+                </article>
+            </section>
+            <section class="row section-criar-nucleo justify-content-center">
+                <article class="col-12 mt-4 mb-3 position-relative">
+                    <h2 class="text-center h2-nucleo_save"> Informação utilizador </h2>
+                </article>
+                <article class="col-12 px-4">
+                    <form action="scripts/sc_criar_nucleo.php" method="post" id="criar_nucleo">
+                        <section class="row justify-content-center">
+                            <article class="col-12">
+                                <div class="div-icons-sign-up text-center position-relative">
+                                    <img id="username_icon" class="icon-novo_admin_1" src="assets/img/input_profile_icon.svg"
+                                         alt="profile_icon">
+                                    <img id="email_icon" class="icon-novo_admin_2" src="assets/img/input_mail_icon.svg"
+                                         alt="profile_icon">
+                                    <img id="email_icon" class="icon-nucleo" src="assets/img/icon_nucleo.svg"
+                                         alt="profile_icon">
+                                </div>
+                                <input id="nome" class="input_backoffice mb-3 mb-md-3 input_edit_user" type="text" name="username"
+                                       size="24" placeholder="username" required="required">
+                                <input id="email" class="input_backoffice mb-3 mb-md-3 input_edit_user" type="email"
+                                       name="email" size="24" placeholder="email" required="required">
+                                <input id="nucleo_pertence" class="input_backoffice mb-3 mb-md-3 input_edit_user" type="text" name="nucleo"
+                                       size="24" placeholder="nucleo" value="" required="required">
+                                <select required="required" class="custom-select select_criar_nucleo mb-3 mb-md-3"
+                                        id="area"
+                                        name="area" form="criar_nucleo">
+                                    <?php
+                                    require_once "connections/connection.php";
+                                    $link = new_db_connection();
+                                    $stmt = mysqli_stmt_init($link);
+                                    $query = "SELECT id_interesse, nome_interesse FROM interesses";
+                                    if (mysqli_stmt_prepare($stmt, $query)) {
+                                        if (mysqli_stmt_execute($stmt)) {
+                                            mysqli_stmt_bind_result($stmt, $id_interesse, $nome_interesse);
+                                            while (mysqli_stmt_fetch($stmt)) {
+                                                echo '<option value="' . $id_interesse . '">' . $nome_interesse . '</option>';
+                                            }
+                                        } else {
+                                            echo "Error:" . mysqli_stmt_error($stmt);
+                                        }
+                                        mysqli_stmt_close($stmt);
+                                    } else {
+                                        echo("Error description: " . mysqli_error($link));
+                                    }
+                                    ?>
+                                </select>
+                                <section class="row justify-content-end sec-bloquear">
+                                        <article id="art_bloquear" class="col-12 check-bloquear">
+                                            <div class="check-bloquear-text"><p id="bloquear-text"> Bloquear </p>
+                                                <img id="ban_cinza" src="assets/img/ban_cinza.svg">
+                                                <img id="ban_vermelho" src="assets/img/ban_vermelho.svg"></div>
+                                            <input id="bloquear" name="bloquear" value="" type="checkbox">
+                                        </article>
+                                </section>
+                            </article>
+                        </section>
+                        <section class="row justify-content-center mt-5 mt-md-3">
+                            <article class="col-md-12 mt-3 mt-md-5 px-4">
+                                <input form="criar_nucleo" type="submit" class="mb-2 mb-md-2" style="display: block;"
+                                       value="salvar" id="criar_nucleo_submit">
+                                <button id="cancelar_criar_nucleo" class="mb-5" type="button"> cancelar</button>
+                            </article>
+                        </section>
+                    </form>
+                </article>
+            </section>
+        </article>
+    </section>
+    <footer class="row justify-content-center py-5">
+        <article class="col-3 text-center">
+            <a href="https://www.facebook.com/" target="_blank"> <span
+                        class="fab fa-facebook-f text-white fa-3x"></span> </a>
+        </article>
+        <article class="col-3 text-center mw-6rem">
+            <a href="https://twitter.com/" target="_blank"> <span class="fab fa-twitter text-white fa-3x"></span> </a>
+        </article>
+        <article class="col-3 text-center">
+            <a href="https://www.instagram.com/" target="_blank"> <span
+                        class="fab fa-instagram text-white fa-3x"></span> </a>
+        </article>
+    </footer>
+</main>
+
+<script>
+
+    checkblock = false;
+    document.getElementById("art_bloquear").onclick = function (){
+        if (checkblock === false){
+            checkblock = true;
+            document.getElementById("bloquear").checked = true;
+            document.getElementById("art_bloquear").style.borderColor = "#F00202";
+            document.getElementById("bloquear-text").style.color = "#F00202";
+            document.getElementById("ban_cinza").style.display = "none";
+            document.getElementById("ban_vermelho").style.display = "inline";
+        } else{
+            checkblock = false;
+            document.getElementById("bloquear").checked = false;
+            document.getElementById("art_bloquear").style.borderColor = "#BCBCBC";
+            document.getElementById("bloquear-text").style.color = "#BCBCBC";
+            document.getElementById("ban_cinza").style.display = "inline";
+            document.getElementById("ban_vermelho").style.display = "none";
+        }
+    }
+
+    document.getElementById("cancelar_criar_nucleo").onclick = function () {
+        window.history.back();
+    }
+</script>
---
+++ components/cp_navegacao_mobile_backoffice.php
@@ -0,0 +1,40 @@
+<nav class="container-fluid nav-mobile">
+    <div class="row nav-mobile-row">
+        <site class="col-3 text-center">
+            <a href="home_page_backoffice.php">
+                <?php if (isset($nav_homepage) && $nav_homepage === true){ ?>
+                    <img src="assets/barra_navegacao/nav_home_filled.svg">
+                <?php } else{ ?>
+                    <img src="assets/barra_navegacao/nav_home.svg">
+                <?php } ?>
+            </a>
+        </site>
+        <site class="col-3 text-center">
+            <a href="backoffice_users.php">
+            <?php if (isset($nav_administradores) && $nav_administradores === true){ ?>
+            <img src="assets/barra_navegacao/nav_administradores_filled.svg">
+            <?php } else{ ?>
+                <img src="assets/barra_navegacao/nav_administradores.svg">
+            <?php } ?>
+            </a>
+        </site>
+        <site class="col-3 text-center">
+            <a href="nucleos.php">
+                <?php if (isset($nav_nucleo) && $nav_nucleo === true){ ?>
+                    <img src="assets/barra_navegacao/nav_nucleo_filled.svg">
+                <?php } else{ ?>
+                    <img src="assets/barra_navegacao/nav_nucleo.svg">
+                <?php } ?>
+            </a>
+        </site>
+        <site class="col-3 text-center">
+            <a href="">
+            <?php if (isset($nav_estatisticas) && $nav_estatisticas === true){ ?>
+            <img src="assets/barra_navegacao/nav_estatisticas_filled.svg">
+            <?php } else{ ?>
+                <img src="assets/barra_navegacao/nav_estatisticas.svg">
+            <?php } ?>
+            </a>
+        </site>
+    </div>
+</nav>
